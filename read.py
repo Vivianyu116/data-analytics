4c2db07ea87b88de149d40d76b9148f6e60d2a3b
Vivian yu <window78116@gmail.com>
2018-08-27 16:33:56 +0800

    add new avg function

--- read.py
+++ read.py
@@ -3,8 +3,13 @@ count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		count=count+1
+		count=count+1 #也等於count += 1
 		if count % 1000 == 0:
 		    print(len(data))
-print(len(data))
-print(data[0])
+print('檔案讀取完了共有', len(data), '數據')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+print('平均是', sum_len/len(data))
